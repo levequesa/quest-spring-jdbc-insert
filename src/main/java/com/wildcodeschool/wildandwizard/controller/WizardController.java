3b53c9d19cd43da7013b4c5af8de99e1275f7173
levequesa <79331585+levequesa@users.noreply.github.com>
2021-05-27 10:04:57 +0200

    maj SchoolController/WizardController/SchoolRepository/application.properties/school_get.html

--- src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
+++ src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
@@ -1,13 +1,13 @@
 package com.wildcodeschool.wildandwizard.controller;
 
-import com.wildcodeschool.wildandwizard.repository.WizardRepository;
+import java.sql.Date;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.sql.Date;
+import com.wildcodeschool.wildandwizard.repository.WizardRepository;
 
 @Controller
 public class WizardController {
--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -1,6 +1,15 @@
 package com.wildcodeschool.wildandwizard.repository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import com.wildcodeschool.wildandwizard.entity.School;
+
+import util.JdbcUtils;
 
 public class SchoolRepository {
 
@@ -10,7 +19,36 @@ public class SchoolRepository {
 
     public School save(String name, Long capacity, String country) {
 
-        // TODO : insert a new school in database
+        Connection connection = null;
+        PreparedStatement request = null;
+        ResultSet generatedKeys = null;
+        
+        try {
+			connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+			request= connection.prepareStatement("insert into school (name,capacity,country) values (?,?,?);", Statement.RETURN_GENERATED_KEYS);
+			request.setString(1, name);
+			request.setLong(2, capacity);
+			request.setString(3, country);
+			
+			if (request.executeUpdate() != 1 ) {
+				throw new SQLException("failed to insert data");
+			}
+			
+			generatedKeys = request.getGeneratedKeys();
+			
+			if (generatedKeys.next()) {
+				Long id = generatedKeys.getLong(1);
+				return new School(id,name,capacity,country);
+			} else {
+				throw new SQLException("failed to get inserted id");
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			JdbcUtils.closeResultSet(generatedKeys);
+			JdbcUtils.closeStatement(request);
+			JdbcUtils.closeConnection(connection);
+		}
         return null;
     }
 }
